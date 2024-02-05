1401c75505e05de161dc003450149fa4bb655708
pawelek <k.pawelek@vp.pl>
2024-02-05 21:33:48 +0100

    hw 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let colorChanger;
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  colorChanger = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  clearInterval(colorChanger);
+  document.body.style.backgroundColor = '';
+});
