d34431062bbc8d99a5d703fec11af1c57a594faa
pawelek <k.pawelek@vp.pl>
2024-02-07 21:05:16 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,16 @@
 import Notiflix from 'notiflix';
-const submitBtn = document.querySelector("button[type='submit']");
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-  });
+const submitBtn = document.querySelector("button[type='submit']");
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(`Promise ${position} resolved`);
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`Promise ${position} rejected`);
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    }
+  }, delay);
+}
