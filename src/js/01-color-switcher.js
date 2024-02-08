c4135585d8a459d5558d642eb5afb686cc9f0bd7
pawelek <k.pawelek@vp.pl>
2024-02-08 12:03:10 +0100

    update-01-hw-styles

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,6 +9,7 @@ let colorChanger;
 
 startBtn.addEventListener('click', () => {
   startBtn.disabled = true;
+  stopBtn.disabled = false;
   colorChanger = setInterval(() => {
     const color = getRandomHexColor();
     document.body.style.backgroundColor = color;
@@ -19,4 +20,5 @@ stopBtn.addEventListener('click', () => {
   startBtn.disabled = false;
   clearInterval(colorChanger);
   document.body.style.backgroundColor = '';
+  stopBtn.disabled = true;
 });
